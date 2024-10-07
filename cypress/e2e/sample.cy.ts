335a4e595d6f2ed08bbbe63a6342bca218edd1d7
Erica Phelps <erica@blackspork.com>
2024-10-06 23:53:33 -0500

    chore: Removing sample test spec

--- cypress/e2e/sample.cy.ts
+++ cypress/e2e/sample.cy.ts
@@ -1,12 +1,12 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('/')
+// describe('template spec', () => {
+//   it('passes', () => {
+//     cy.visit('/')
 
-      .selectWorld("blank-test-world")
-      .selectUser("Gamemaster")
-      .wait(5000)
-      .clickMacroButton(1)
-      .wait(10000)
+//       .selectWorld("blank-test-world")
+//       .selectUser("Gamemaster")
+//       .wait(5000)
+//       .clickMacroButton(1)
+//       .wait(10000)
 
-  })
-})
+//   })
+// })
