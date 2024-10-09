37a947674db1beed6122103ea75c5ddd8ea1b413
Erica Phelps <erica@blackspork.com>
2024-10-09 17:54:38 -0500

    feat:Bootstrapped xstate

--- src/lib/ActorDock/interfaces.ts
+++ src/lib/ActorDock/interfaces.ts
@@ -1,4 +1,11 @@
+import { Position, Size } from '../interfaces';
 export interface ActorDockState {
   name: string;
   image: string;
+
+  position?: Position,
+  size?: Size,
+
+  portrait: Tile,
+  textbox: Tile
 }
