52583d55b83c9f8b01452359bd42d3290a1b31c0
Erica Phelps <erica@blackspork.com>
2024-10-12 04:12:47 -0500

    feat:Added StageObject, ImageStageObject, TextStageObject

--- src/lib/StageObjects/ImageStageObject.ts
+++ src/lib/StageObjects/ImageStageObject.ts
@@ -1,22 +1,68 @@
 import { StageObject } from "./StageObject";
 
-
 export class ImageStageObject extends StageObject {
-  public displayObject: PIXI.Sprite;
 
-  // eslint-disable-next-line no-unused-private-class-members
-  #sizeChange$ = this.size$.pipe(
-    rxjs.takeUntil(this.destroy$)
-  ).subscribe(size => {
-    if (this.displayObject) {
-      this.displayObject.width = size.width;
-      this.displayObject.height = size.height;
-    }
-  })
+  protected override _displayObject: PIXI.Sprite;
+  public override get displayObject() { return this._displayObject; }
 
-  constructor(public readonly source: PIXI.SpriteSource) {
-    super();
-    this.displayObject = PIXI.Sprite.from(source);
-    this.displayObject.visible = false;
+  //#region Sizing
+  #width = new rxjs.BehaviorSubject<number>(0);
+  public get width(): number { return this.displayObject.width; }
+  public set width(value: number) {
+    this.displayObject.width = value;
+    this.#width.next(value);
+  }
+  public readonly width$ = this.#width.asObservable();
+
+  #height = new rxjs.BehaviorSubject<number>(0);
+  public get height(): number { return this.displayObject.height; }
+  public set height(value: number) {
+    this.displayObject.height = value;
+    this.#height.next(value);
+  }
+  public readonly height$ = this.#height.asObservable();
+  //#endregion
+
+  //#region Drawing
+  #blendMode = new rxjs.BehaviorSubject<PIXI.BLEND_MODES>(PIXI.BLEND_MODES.NORMAL);
+  public get blendMode(): PIXI.BLEND_MODES { return this.displayObject.blendMode; }
+  public set blendMode(value: PIXI.BLEND_MODES) {
+    this.displayObject.blendMode = value;
+    this.#blendMode.next(value);
+  }
+  public readonly blendMode$ = this.#blendMode.asObservable();
+
+  #tint = new rxjs.BehaviorSubject<PIXI.ColorSource>(0xFFFFFF);
+  public get tint(): PIXI.ColorSource { return this.displayObject.tint; }
+  public set tint(value: PIXI.ColorSource) {
+    this.displayObject.tint = value;
+    this.#tint.next(value);
+  }
+  public readonly tint$ = this.#tint.asObservable();
+
+  #roundPixels = new rxjs.BehaviorSubject<boolean>(false);
+  public get roundPixels(): boolean { return this.displayObject.roundPixels; }
+  public set roundPixels(value: boolean) {
+    this.displayObject.roundPixels = value;
+    this.#roundPixels.next(value);
+  }
+  public readonly roundPixels$ = this.#roundPixels.asObservable();
+
+  //#endregion
+
+  #anchor = new rxjs.BehaviorSubject<Point>({ x: 0, y: 0 });
+  public get anchor(): Point { return this.displayObject.anchor; }
+  public set anchor(value: Point) {
+    this.displayObject.anchor.x = value.x;
+    this.displayObject.anchor.y = value.y;
+    this.#anchor.next(value);
+  }
+  public readonly anchor$ = this.#anchor.asObservable();
+
+
+  constructor(source: PIXI.SpriteSource) {
+    const sprite = PIXI.Sprite.from(source);
+    super(sprite);
+    this._displayObject = sprite;
   }
 }
--- src/lib/StageObjects/TextStageObject.ts
+++ src/lib/StageObjects/TextStageObject.ts
@@ -1,26 +1,110 @@
+import { Size } from "../interfaces";
 import { StageObject } from "./StageObject";
 
-// export type TextStyle = PIXI.TextStyle | PIXI.HTMLTextStyle;
-
 export class TextStageObject extends StageObject {
-  public displayObject: PIXI.HTMLText;
+  #displayObject: PIXI.HTMLText;
+  public get displayObject() { return this.#displayObject; }
 
-  #text$ = new rxjs.BehaviorSubject<string>("");
-
-  public get text() { return this.#text$.value; }
-  public set text(value: string) { this.#text$.next(value); }
-  public readonly text$ = this.#text$.asObservable().pipe(
-    rxjs.takeUntil(this.destroy$),
-    rxjs.distinctUntilChanged()
-  );
-
-  public style: PIXI.ITextStyle = PIXI.TextStyle.defaultStyle;
-
-  constructor(text: string, style?: PIXI.ITextStyle) {
-    super();
-    this.#text$.next(text);
-    if (style) this.style = style;
-    this.displayObject = new PIXI.HTMLText(text, style);
+  //#region Sizing
+  #width = new rxjs.BehaviorSubject<number>(0);
+  public get width(): number { return this.displayObject.width; }
+  public set width(value: number) {
+    this.displayObject.width = value;
+    this.#width.next(value);
   }
+  public readonly width$ = this.#width.asObservable();
 
+  #height = new rxjs.BehaviorSubject<number>(0);
+  public get height(): number { return this.displayObject.height; }
+  public set height(value: number) {
+    this.displayObject.height = value;
+    this.#height.next(value);
+  }
+  public readonly height$ = this.#height.asObservable();
+
+  #maxWidth = new rxjs.BehaviorSubject<number>(0);
+  public get maxWidth(): number { return this.displayObject.maxWidth; }
+  public set maxWidth(value: number) {
+    this.displayObject.maxWidth = value;
+    this.#maxWidth.next(value);
+  }
+  public readonly maxWidth$ = this.#maxWidth.asObservable();
+
+  #maxHeight = new rxjs.BehaviorSubject<number>(0);
+  public get maxHeight(): number { return this.displayObject.maxHeight; }
+  public set maxHeight(value: number) {
+    this.displayObject.maxHeight = value;
+    this.#maxHeight.next(value);
+  }
+  public readonly maxHeight$ = this.#maxHeight.asObservable();
+
+  #resolution = new rxjs.BehaviorSubject<number>(1);
+  public get resolution(): number { return this.displayObject.resolution; }
+  public set resolution(value: number) {
+    this.displayObject.resolution = value;
+    this.#resolution.next(value);
+  }
+  public readonly resolution$ = this.#resolution.asObservable();
+  //#endregion
+
+  //#region Text
+  #text = new rxjs.BehaviorSubject<string>("");
+  public get text(): string { return this.displayObject.text; }
+  public set text(value: string) {
+    this.displayObject.text = value;
+    this.#text.next(value);
+  }
+  public readonly text$ = this.#text.asObservable();
+
+  #style = new rxjs.BehaviorSubject<PIXI.HTMLTextStyle>(PIXI.HTMLTextStyle.defaultStyle as PIXI.HTMLTextStyle);
+  public get style(): PIXI.HTMLTextStyle { return this.displayObject.style; }
+  public set style(value: PIXI.HTMLTextStyle) {
+    this.displayObject.style = value;
+    this.#style.next(value);
+  }
+  public readonly style$ = this.#style.asObservable();
+
+  //#endregion
+
+  //#region Methods
+  public measureText() {
+    return this.displayObject.measureText() as Size;
+  }
+  //#endregion
+
+  //#region Construction
+  constructor(text: string, style?: PIXI.HTMLTextStyle) {
+    const textObj = new PIXI.HTMLText(text, style);
+    super(textObj);
+    this.#displayObject = textObj;
+
+  }
+  //#endregion
 }
+
+// import { StageObject } from "./StageObject";
+
+// // export type TextStyle = PIXI.TextStyle | PIXI.HTMLTextStyle;
+
+// export class TextStageObject extends StageObject {
+//   public displayObject: PIXI.HTMLText;
+
+//   #text$ = new rxjs.BehaviorSubject<string>("");
+
+//   public get text() { return this.#text$.value; }
+//   public set text(value: string) { this.#text$.next(value); }
+//   public readonly text$ = this.#text$.asObservable().pipe(
+//     rxjs.takeUntil(this.destroy$),
+//     rxjs.distinctUntilChanged()
+//   );
+
+//   public style: PIXI.HTMLTextStyle = PIXI.TextStyle.defaultStyle;
+
+//   constructor(text: string, style?: PIXI.HTMLTextStyle) {
+//     super();
+//     this.#text$.next(text);
+//     if (style) this.style = style;
+//     this.displayObject = new PIXI.HTMLText(text, style);
+//   }
+
+// }
