8fbfe7c8dc961596ad1deb339f29b4bd205abc19
Erica Phelps <erica@blackspork.com>
2025-03-30 04:38:44 -0500

    feat: Added bringToFront input handler

--- src/input/index.ts
+++ src/input/index.ts
@@ -1,10 +1,12 @@
 import { KeyboardEventContext } from "Foundry-VTT/src/foundry/common/types.mjs";
 import dismiss from "./dismiss";
 import characterSheet from "./characterSheet";
+import bringToFront from "./bringToFront";
 
 export type HandlerFunction = (context: KeyboardEventContext) => boolean | Promise<boolean>;
 
 export const InputHandlers: Record<string, HandlerFunction> = {
   "core.dismiss": dismiss,
-  "core.characterSheet": characterSheet
+  "core.characterSheet": characterSheet,
+  "core.bringToFront": bringToFront
 }
