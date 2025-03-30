f5413dffc5b351a5e3c5baa1a850ce67c2d54d96
Erica Phelps <erica@blackspork.com>
2025-03-30 05:19:33 -0500

    fix: Added check for canUserModify to edit handler

--- src/input/characterSheet.ts
+++ src/input/characterSheet.ts
@@ -1,10 +1,15 @@
+import { PermissionDeniedError } from "errors";
+import { logError } from "logging";
 import {StageManager} from "StageManager";
 
 function characterSheet(context: KeyboardManager.KeyboardEventContext): boolean {
   
   if (StageManager.StageObjects.selected.length === 1) {
     if (context.up) return false;
-    void StageManager.EditStageObject(StageManager.StageObjects.selected[0]);
+    if (StageManager.StageObjects.selected[0].canUserModify(game.user as User, "modify"))
+      void StageManager.EditStageObject(StageManager.StageObjects.selected[0]);
+    else
+      logError(new PermissionDeniedError());
     return true;
   } else {
     return false;
